bac31414e11a7f14525226938dff97f4d7ce67db
Naseel Niyas <naseel@techharvesting.in>
2021-12-24 15:27:53 +0530

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Try
+            Label3.Text = Integer.Parse(TextBox1.Text) + Integer.Parse(TextBox2.Text)
+        Catch ex As Exception
+            Label3.Text = ex.Message
+        End Try
+    End Sub
+End Class
